532d95928c13f247eef45b8f874fbd164daa4b0a
kinyuaeric <nd.kinyuaeric@gmail.com>
2022-06-16 21:18:43 +0300

    updates

---
+++ om_library/__manifest__.py
@@ -0,0 +1,19 @@
+{
+'name': 'Library Management System',
+'description': 'Manage library book catalogue and lending',
+'author': 'Eric Kinyua',
+'category': 'extra tools',
+'depends': ['base'],
+'application': True,
+'installable': True,   
+'data': [
+  'security/library_security.xml',
+  'security/ir.model.access.csv',
+  'views/library_menu.xml', 
+  'views/book_view.xml', 
+  'views/book_list_template.xml',
+  'reports/library_book_report.xml',
+  'reports/library_book_sql_report.xml',
+
+  ]
+}
