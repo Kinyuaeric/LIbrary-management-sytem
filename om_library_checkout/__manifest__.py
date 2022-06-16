61e32e702b9454ab8f707c8d75cc2ad413172ce8
kinyuaeric <nd.kinyuaeric@gmail.com>
2022-06-16 23:58:40 +0300

    add checkout module

---
+++ om_library_checkout/__manifest__.py
@@ -0,0 +1,10 @@
+{'name': 'Library Book Borrowing',
+ 'description': 'Members can borrow books from the library.',
+ 'author': 'eric kinyua',
+ 'depends': ['library_member'],
+ 'data': [
+     'security/ir.model.access.csv',
+     'views/lib_menu.xml',
+     'views/check_out.xml',
+ ],
+ }
